3bcea7c1490160953007ec6ae9f8f16159414120
frmad <frmad21@student.sdu.dk>
2024-08-26 12:09:44 +0200

    fix routing

---
+++ src/routes/RecipeCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RecipeCard() {
+    return (
+        <div>
+            <h1>Sub Page</h1>
+            <p>Welcome to the Sub Page.</p>
+        </div>
+    );
+}
+
+export default RecipeCard;
